cfac31216b75f808d367ddc94e9257640071576c
@piducancore <hola@piducan.dev>
2022-07-01 12:37:36 -0400

    v0.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,202 @@
-import * as React from "react"
+import React from "react";
+import barCode from "../images/barcode.svg";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+import "../styles.css";
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+export default function IndexPage() {
+  const now = new Date();
+  const days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+  const months = [
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "mayo",
+    "junio",
+    "julio",
+    "agosto",
+    "septiembre",
+    "octubre",
+    "noviembre",
+    "diciembre",
+  ];
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+  const { day, number, month, year } = {
+    day: days[now.getDay()],
+    number: now.getDate(),
+    month: months[now.getMonth()],
+    year: now.getFullYear(),
+  };
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+  const [file, setFile] = React.useState(null);
+  const [fileDataURL, setFileDataURL] = React.useState(null);
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+  // const imageMimeType = /image\/(png|jpg|jpeg)/i;
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+  const onFileChange = (e) => {
+    const file = e.target.files[0];
+    // if (!file?.type.match(imageMimeType)) {
+    //   alert("Image mime type is not valid");
+    //   return;
+    // }
+    setFile(file);
+  };
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
-const IndexPage = () => {
+  React.useEffect(() => {
+    let fileReader,
+      isCancel = false;
+    if (file) {
+      fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        const { result } = e.target;
+        if (result && !isCancel) {
+          setFileDataURL(result);
+        }
+      };
+      fileReader.readAsDataURL(file);
+    }
+    return () => {
+      isCancel = true;
+      if (fileReader && fileReader.readyState === 1) {
+        fileReader.abort();
+      }
+    };
+  }, [file]);
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+    <React.Fragment>
+      <header>
+        <h1
+          style={{
+            borderTop: "2vw solid #ff001c",
+            background: "#31006f",
+            color: "white",
+            fontFamily: "Gill Sans Display MT Pro",
+            textAlign: "center",
+            paddingTop: "3vw",
+            fontSize: "8vw",
+            lineHeight: "8vw",
+            margin: 0,
+          }}
+        >
+          <span contentEditable="true" spellCheck="false" suppressContentEditableWarning={true}>
+            Las Últimas Noticias
+          </span>
+        </h1>
+        <div style={{ display: "flex", padding: "0.2vw 0" }}>
+          <p
+            style={{
+              fontSize: "2vw",
+              margin: "auto",
+            }}
           >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+            <strong>cyberpun.ga/lun</strong>
+          </p>
+          <p
+            style={{
+              fontSize: "2vw",
+              margin: "auto",
+            }}
+          >
+            $500 • Año CXX • N° 40.330 • {day} {number} de {month} {year}
+          </p>
+          <img
+            alt="cyberpunga barcode"
+            style={{
+              height: "2.4vw",
+              padding: "0.5vw 0.5vw",
+              border: "0.1vw solid darkgray",
+              borderRadius: "0.5vw",
+              margin: "auto 0",
+            }}
+            src={barCode}
+          />
+        </div>
+      </header>
+      <main
+        style={{
+          backgroundImage: `url(${
+            fileDataURL ? fileDataURL : "https://clipground.com/images/placeholder-clipart-5.jpg"
+          })`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+          height: "100vw",
+        }}
+      >
+        <div style={{ margin: "auto" }}>
+          <label
+            for="file"
+            style={{ border: "1px dashed black", borderRadius: "4px", padding: "8px", cursor: "pointer" }}
+          >
+            Cambiar imagen
+          </label>
+          <input type="file" accept="image/*" onChange={onFileChange} id="file" hidden />
+        </div>
 
-export default IndexPage
+        <h1
+          style={{
+            fontFamily: "Gill Sans Display MT Pro",
+            textAlign: "center",
+            margin: "auto 0 0 0",
+            color: "yellow",
+            fontSize: "10vw",
+            lineHeight: "1",
+            WebkitTextStroke: "0.2vw black",
+          }}
+        >
+          <span contentEditable="true" spellCheck="false" suppressContentEditableWarning={true}>
+            Un nuevo generador de noticias falsas se suma a la lista
+          </span>
+        </h1>
+
+        <div
+          style={{
+            background: "#ff001c",
+            color: "white",
+            borderTop: "0.1vw solid white",
+            borderRadius: "0.5vw 0.5vw 0 0",
+            display: "flex",
+            alignItems: "center",
+            padding: "4vw",
+          }}
+        >
+          <p
+            style={{
+              fontFamily: "Gill Sans Display MT Pro",
+              fontSize: "4vw",
+              margin: 0,
+              lineHeight: "4vw",
+            }}
+          >
+            <span contentEditable="true" spellCheck="false" suppressContentEditableWarning={true}>
+              Si estás leyendo esto, significa que sigues con{" "}
+            </span>
+            <span>4️⃣</span>
+          </p>
+          <div
+            style={{
+              borderLeft: "0.1vw solid white",
+              height: "16vw",
+              margin: "0 4vw",
+            }}
+          ></div>
+          <p
+            style={{
+              fontFamily: "Gill Sans Display MT Pro",
+              fontSize: "4vw",
+              margin: 0,
+              lineHeight: "4vw",
+            }}
+          >
+            <span contentEditable="true" spellCheck="false" suppressContentEditableWarning={true}>
+              Estos textos son editables: así se recupera de cirugía a las cuerdas vocales
+            </span>
+            <span>8️⃣</span>
+          </p>
+        </div>
+      </main>
+    </React.Fragment>
+  );
+}
